20f5ab3c5a559020ee73d25cef5b16d31174d7f6
hippoom <hippoom@gmail.com>
2017-05-07 00:40:55 +0800

    fix space in workspace

--- scripts/deploy-uat.sh
+++ scripts/deploy-uat.sh
@@ -3,4 +3,4 @@
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 project_home="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../" && pwd )"
 
-ansible-playbook -i profiles/uat/hosts playbooks/itzero.yml -e "WORKSPACE=${project_home}" -e "PROFILE=uat"
+ansible-playbook -i profiles/uat/hosts playbooks/itzero.yml -e "WORKSPACE=\"${project_home}\"" -e "PROFILE=uat"
