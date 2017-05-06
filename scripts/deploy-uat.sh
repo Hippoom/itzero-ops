18d2b1115d8868c31fe9dc7cbaccdfcaf498f212
hippoom <hippoom@gmail.com>
2017-05-07 00:18:59 +0800

    deploy scripts

---
+++ scripts/deploy-uat.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+project_home="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../" && pwd )"
+
+ansible-playbook -i profiles/uat/hosts playbooks/itzero.yml -e "WORKSPACE=${project_home}" -e "PROFILE=uat"
